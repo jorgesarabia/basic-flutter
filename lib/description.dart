a072f3224e1aca7a6f75a852f36eb498e90aacda
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-20 18:00:27 -0300

    Clase 20

---
+++ lib/description.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Description extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            "Jorge",
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star
+          ],
+        ),
+      ],
+    );
+
+    return title_stars;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:basic_flutter/description.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,33 +16,7 @@ class MyApp extends StatelessWidget {
           title: Text("Segundo Reto"),
           leading: BackButton(),
         ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              child: Image.network(
-                'http://lorempixel.com/640/480',
-                height: double.infinity,
-                width: double.infinity,
-                fit: BoxFit.fill,
-                ),
-            ),
-            Center(
-              child: Container(
-                color: Colors.black.withOpacity(0.5),
-                width: double.infinity,
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Segundo Reto",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            )
-          ],
-        ),
+        body: new Description(),
       ),
     );
   }
