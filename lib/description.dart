bda9d52e9543402576b650aca006447b2a7f7ec7
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-20 18:31:56 -0300

    Se prepara la clase como para que reciba nombres, estrellas y descripciones de forma dinamica

--- lib/description.dart
+++ lib/description.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
 
 class Description extends StatelessWidget{
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  Description(this.namePlace, this.stars, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
+
+    final description = Container(
+      margin: EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0,
+      ),
+      child: Text(
+        descriptionPlace,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a),
+        ),
+      ),
+    );
+
+    final star_half = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
 
     final star = Container(
       margin: EdgeInsets.only(
@@ -24,7 +69,7 @@ class Description extends StatelessWidget{
             right: 20.0,
           ),
           child: Text(
-            "Jorge",
+            namePlace,
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.w900,
@@ -37,14 +82,19 @@ class Description extends StatelessWidget{
             star,
             star,
             star,
-            star,
-            star
+            star_half,
+            star_border
           ],
         ),
       ],
     );
 
-    return title_stars;
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
+      ],
+    );
   }
 
 }
